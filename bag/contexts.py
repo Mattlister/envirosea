adc08bcf8a69b86bcf23a51f035e88ff17cc0227
Matthew Lister <mdlister24@hotmail.com>
2020-07-17 11:01:10 +0000

    Quantity options and buttons added

--- bag/contexts.py
+++ bag/contexts.py
@@ -30,7 +30,7 @@ def bag_contents(request):
                     'item_id': item_id,
                     'quantity': item_data,
                     'product': product,
-                    'size':size,
+                    'size': size,
                 })
 
 
