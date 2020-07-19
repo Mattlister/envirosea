7e673d888bbdbf3f08fc6d0a0ba41f32d85045ac
Matthew Lister <mdlister24@hotmail.com>
2020-07-19 12:58:37 +0000

    Links and checkout form created

--- envirosea/settings.py
+++ envirosea/settings.py
@@ -47,6 +47,12 @@ INSTALLED_APPS = [
     'checkout',
 
 
+# Other
+'crispy_forms',
+
+
+
+
 ]
 
 MIDDLEWARE = [
@@ -60,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'envirosea.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,6 +85,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+           'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
