bbec58b83d1958f7cefd8c90efd0f575edd42b92
Matthew Lister <mdlister24@hotmail.com>
2020-07-19 19:37:09 +0000

    Working on stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51GrLVxJPBbER9O2LCK7dZJqBFzZKoatqszYdSmJMOwGssGCItfajDGGF3dbhgY6BhC2ViwpGXJFNZLX8EeFgCv7M00KNt8MtIA',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
