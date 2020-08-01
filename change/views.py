79480da16e77099ebab91cecff284ba3e24334dc
Matthew Lister <mdlister24@hotmail.com>
2020-08-01 11:10:55 +0000

    Creating views for new pages

--- change/views.py
+++ change/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def change(request):
+    """ View to return change page """
+    return render(request, 'change/why.html')
--- ocean/views.py
+++ ocean/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def oceans(request):
+    """ View to return oceans page """
+    return render(request, 'ocean/oceans.html')
