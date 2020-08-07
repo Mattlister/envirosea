1433cecfe17a490d17851d3943f90079695a413f
Matthew Lister <mdlister24@hotmail.com>
2020-08-07 09:43:31 +0000

    Setting up booking bag

---
+++ book/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def view_book(request):
+    """ View to renders the book contents page"""
+    
+    return render(request, 'book/book.html')
--- envirosea/settings.py
+++ envirosea/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'book',
     'checkout',
     'profiles',
     'bookings',
