0f56b32b3f7d6b3468750e4fe6ad427a7feb20a0
Matthew Lister <mdlister24@hotmail.com>
2020-08-08 16:19:03 +0000

    Added add person functionality

--- book/urls.py
+++ book/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_book, name='view_book'),
+    path('add/<item_id>/', views.add_person, name='add_person'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def view_book(request):
-    """ View to renders the book contents page"""
+    """ A view that adds people to booking """
+
     return render(request, 'book/book.html')
+
+
+def add_person(request, item_id):
+    """ Add a quantity of people to the booking """
+
+    quantity1 = int(request.POST.get('quantity1'))
+
+    redirect_url = request.POST.get('redirect_url')
+    book = request.session.get('book', {})
+
+    if item_id in list(book.keys()):
+        book[item_id] += quantity1
+    else:
+        book[item_id] = quantity1
+
+    request.session['book'] = book
+    print(request.session['book'])
+    return redirect(redirect_url)
