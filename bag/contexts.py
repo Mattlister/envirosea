48c1dbd9e860d9ab9d76c1c6877336f53b8b49e1
Matthew Lister <mdlister24@hotmail.com>
2020-08-08 13:07:01 +0000

    Bookings page now loading

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,22 +5,16 @@ from products.models import Product
 
 
 def bag_contents(request):
-    delivery = 0
-    free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD
-    bag_items = []
-    booking_items = []
-    bag_total = 0
-    booking_total = 0
-    product_count = 0
-    booking_count = 0
-    bag = request.session.get('bag', {})
-    booking = request.session.get('book', {})
 
-    # Your Loop For Products
+    bag_items = []
+    total = 0
+    product_count = 0
+    bag = request.session.get('bag', {})
+
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            bag_total += item_data * product.price
+            total += item_data * product.price
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
@@ -30,53 +24,33 @@ def bag_contents(request):
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                bag_total += item_data * product.price
+                total += item_data * product.price
                 product_count += item_data
                 bag_items.append({
-                        'item_id': item_id,
-                        'quantity': quantity,
-                        'product': product,
-                        'size': size,
-                    })
-
-        if bag_total < settings.FREE_DELIVERY_THRESHOLD:
-            delivery = bag_total * \
-                Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-            free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - bag_total
-        else:
-            delivery = 0
-            free_delivery_delta = 0
-
-        # YOUR LOOP FOR SERVICES GOES HERE
-
-    for item_id, item_data in booking.items():
-        if isinstance(item_data, int):
-            booking = get_object_or_404(Booking, pk=item_id)
-            booking_total += item_data * Booking.price
-            booking_count += item_data
-            booking_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
-                    'booking': booking,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
                 })
-        else:
-            booking = get_object_or_404(booking, pk=item_id)
 
-        if bag_total < settings.FREE_DELIVERY_THRESHOLD:
-            delivery = bag_total * \
-                Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-            free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - booking_total
-        # you can get rid of the else here
-        # else:
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
 
     context = {
-        'booking_items': booking_items,
-        'booking_count': booking_count,
-        'booking_total': booking_total,
         'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
     }
 
     return context
--- book/views.py
+++ book/views.py
@@ -5,5 +5,4 @@ from django.shortcuts import render
 
 def view_book(request):
     """ View to renders the book contents page"""
-
     return render(request, 'book/book.html')
