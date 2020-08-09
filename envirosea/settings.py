a27f4871bfc79ae2d429fe862616a95f7b8bc1e1
Matthew Lister <mdlister24@hotmail.com>
2020-08-09 21:21:52 +0000

    Heroku setup issues

--- envirosea/settings.py
+++ envirosea/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@tn*9$m9^ox8=a8nr$4s+!g)#!n#84@(2ks#1jo$w+6st7u+&'
+SECRET_KEY = os.environ.get()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
