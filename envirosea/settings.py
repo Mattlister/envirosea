442212da1349af870e8c42140d5bc1d530d78f23
Matthew Lister <mdlister24@hotmail.com>
2020-08-10 07:43:46 +0000

    Resolving heroku push issues

--- envirosea/settings.py
+++ envirosea/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get()
+SECRET_KEY = os.environ.get('SECRET_KEY', '=@tn*9$m9^ox8=a8nr$4s+!g)#!n#84@(2ks#1jo$w+6st7u+&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -130,17 +130,18 @@ WSGI_APPLICATION = 'envirosea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# if 'DATABASE_URL' in os.environ:
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -159,7 +160,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
