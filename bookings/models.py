e896c486e06601369d34fcdddd277773ccb4afe0
Matthew Lister <mdlister24@hotmail.com>
2020-08-20 18:23:43 +0000

    Added verbose name to change tripss to trips in admin

--- bookings/models.py
+++ bookings/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Trips(models.Model):
 
+    class Meta:
+        verbose_name_plural = 'Trips'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
