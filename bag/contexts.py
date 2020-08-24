f57959068a5bd3f933fa91b9196bdadd30c72466
Matthew Lister <mdlister24@hotmail.com>
2020-08-24 18:05:28 +0000

    Added booking total to shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,6 +9,7 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    person_count = 0
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
@@ -41,15 +42,18 @@ def bag_contents(request):
         free_delivery_delta = 0
 
     grand_total = delivery + total
+    booking_total = total
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
+        'person_count': person_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
+        'booking_total': booking_total,
     }
 
     return context
