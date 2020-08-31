56b723b25189f9dd81899df0fe8a6aea2be5a4f6
Matthew Lister <mdlister24@hotmail.com>
2020-08-31 18:29:46 +0000

    Removing info not needed due to changes to bookings

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.all_bookings, name='bookings'),
     path('<int:booking_id>', views.booking_detail, name='booking_detail'),
-    path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
-    path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
 ]
