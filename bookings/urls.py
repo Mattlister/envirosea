31e7d8a06b66a3a69e0fbf078f4798e82155b1e1
Matthew Lister <mdlister24@hotmail.com>
2020-08-31 17:25:54 +0000

    Removing files as changing setup due to unresolved issues in timeframe

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.all_bookings, name='bookings'),
     path('<int:booking_id>', views.booking_detail, name='booking_detail'),
-    path('add/', views.add_booking, name='add_booking'),
     path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
     path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
 ]
